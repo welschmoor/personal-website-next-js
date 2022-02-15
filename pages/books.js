ef0a439f2f6fe4814ed67040a75b361c72a99556
Johannes Welsch <welschmoor@gmail.com>
2022-02-15 02:24:28 +0100

    changed color for /books

--- pages/books.js
+++ pages/books.js
@@ -2,17 +2,25 @@
 // I don't use a backend, that's why I use an array here.
 
 import Head from "next/head"
+import styled from "styled-components"
 
 
 const Books = () => {
   return (
-    <div>
+    <BooksWrapper>
       <Head><title>Johannes Welsch - Books</title></Head>
-      <div>List of books I've read in the last years</div>
-      <div> - under construction 13. Feb. 2022</div>
-    </div>
+      <P>List of books I've read in the last years</P>
+      <P> - under construction 13. Feb. 2022</P>
+    </BooksWrapper>
   )
 }
 
+const BooksWrapper = styled.div`
+  color: ${p => p.theme.TEXT.mainNav};
+`
+
+const P = styled.p`
+color: ${p => p.theme.TEXT.mainNav};
+`
 
 export default Books
