654dd2c66e0011b8fad653b22f151392d10a0b87
Johannes Welsch <welschmoor@gmail.com>
2022-02-15 18:17:37 +0100

    added active navbar element

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -6,10 +6,12 @@ import styled from "styled-components"
 
 const Navbar = () => {
   const router = useRouter()
-  console.log(router)
+  console.log('router', router)
+  const path = router.pathname
+
   return (
     <Nav>
-      <Link href="/recipes" passHref><A>Recipes</A></Link>
+      <Link href="/recipes" passHref><A><FatDot path={path} />Recipes</A></Link>
       <Link href="/projects" passHref><A>Projects</A></Link>
       <Link href="/about" passHref><A>About</A></Link>
     </Nav>
@@ -18,18 +20,21 @@ const Navbar = () => {
 
 
 const A = styled.a`
+  position: relative;
+
   text-transform: uppercase;
   text-decoration: none;
   color: ${p => p.theme.TEXT.mainNav};
   font-weight: 700;
   cursor: pointer;
-
-  border-bottom: 3px solid transparent;
   padding: 10px 0;
+  border-bottom: 3px solid transparent;
+  padding-bottom: 10px;
   transition: padding ${p => p.theme.TIME.zero2};;
 
   &:hover {
-    padding: 4px 0;
+    padding-bottom: 5px;
+
     border-bottom: 3px solid #7a7875;
   }
 
@@ -42,7 +47,7 @@ const A = styled.a`
 const Nav = styled.nav`
   margin-right: 0px;
   display: flex;
-  align-items: center;
+  /* align-items: center; */
   justify-content: end;
   gap: 25px;
 
@@ -50,6 +55,22 @@ const Nav = styled.nav`
 
     justify-content: center;
   }
+`
+
+const FatDot = styled.div`
+  display:  ${p => p.path === "/" ? "none" : "normal"};
+  top: -18px;
+  left: ${p => p.path === "/recipes" 
+  ? "30px" 
+  : p.path === "/projects" ? "138px" : "243px"
+  };
+  position: absolute;
+  padding: 7px;
+  border-radius: 50%;
+  /* background-color: ${p => p.theme.TEXT.mainNav}; */
+  border: 3px solid ${p => p.theme.TEXT.mainNav};
+
+
 `
 
 export default Navbar
